25c1a7c73cee5c4c8b6a7db0465a23dd9f272632
ncip48 <mbahcip00@gmail.com>
2024-05-30 23:25:37 +0700

    migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('id_kereta');
             $table->string('nip')->unique();
             $table->integer('id_kereta');
             $table->string('name');
